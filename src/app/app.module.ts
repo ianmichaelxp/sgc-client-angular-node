0fd0a3252efe7d3ba56d9d3023f386f368176392
Ian <ianmichael.xp@gmail.com>
2021-05-04 16:21:14 -0300

    componente adicionar-contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NovoContatoComponent } from './novo-contato/novo-contato.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovoContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/novo-contato/novo-contato.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-contato',
+  templateUrl: './novo-contato.component.html',
+  styleUrls: ['./novo-contato.component.scss']
+})
+export class NovoContatoComponent {
+
+  @Output() aoSalvar = new EventEmitter<any>();
+  nome: string;
+  sobrenome: string;
+  email: string;
+  numero: string;
+
+  salvar(){
+    console.log('Solicitada nova transferência');
+    const contato = { nome: this.nome, sobrenome: this.sobrenome, email: this.email, numero: this.numero };
+    this.aoSalvar.emit(contato);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.nome = "";
+    this.sobrenome = "";
+    this.email = "";
+    this.numero = "";
+  }
+
+
+}
